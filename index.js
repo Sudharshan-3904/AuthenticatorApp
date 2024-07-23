74a8325e5eb4ccb86a2c9ed2f540f8aef58f3a32
Sudharshan-3904 <sudharshanvalar@gmail.com>
2024-07-23 15:19:24 +0530

    23072-24-Commit-02

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#userName").text(profile.getName());
-    $("#profile_pic").attribute("src", profile.getImageUrl());
-    $("#Email").text(profile.getEmail());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
-    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // $("#userName").text(profile.getName());
+    // $("#profile_pic").attribute("src", profile.getImageUrl());
+    // $("#Email").text(profile.getEmail());
+    // $(".data").css("display", "block");
+    // $(".g-signin2").css("display", "none");
+    // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log("Name: " + profile.getName());
+    // console.log("Image URL: " + profile.getImageUrl());
+    // console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    window.location.href = "./logged_in.html";
 }
 
 function signOut() {
